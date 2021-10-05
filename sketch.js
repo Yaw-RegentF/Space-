f5723522c274cb24ff3e7db35164b27a8c2dc8d3
Yaw-RegentF <87759009+Yaw-RegentF@users.noreply.github.com>
2021-10-05 18:57:32 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var bath,brush,drink,eat,gym1,gym2,iss,move,sleep;
+var bathImg,brushImg,drinkImg,eatImg,gym1Img,gym2Img,issImg,moveImg,sleepImg;
+
+function setup() {
+  createCanvas(400, 400);
+
+  bathImg = loadAnimation("")
+}
+
+function draw() {
+  background(220);
+}
